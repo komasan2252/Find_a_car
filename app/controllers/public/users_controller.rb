a2c34579cd7066837150e3e376a630742f14ea03
komasan2252 <tomoki.momo.2252@gmail.com>
2025-06-10 02:32:26 +0000

    ユーザーの編集削除

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,14 +1,41 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "更新しました"
+      redirect_to @user
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
+    @user&.destroy
+    flash[:notice] = "退会しました"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:family_size, :monthly_mileage)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path if current_user != @user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }, on: :create
+  validates :family_size, presence: true, numericality: { only_integer: true }, on: :update
+  validates :monthly_mileage, presence: true, numericality: { only_integer: true }, on: :update
 end
